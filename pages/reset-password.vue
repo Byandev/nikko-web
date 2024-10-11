052faf433efa25b785ffb5501f6e1d28d2147e87
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2024-10-11 11:38:53 +0800

    improve ui/ux

--- pages/reset-password.vue
+++ pages/reset-password.vue
@@ -4,6 +4,7 @@ import { useRouter } from 'vue-router';
 import { Icon } from '@iconify/vue';
 import type { ApiErrorResponse } from '~/types/api/response/error';
 import { useResetPasswordStore } from '~/store/resetPasswordStore';
+import { maskEmail } from '~/utils/helper';
 
 definePageMeta({
     auth: {
@@ -16,10 +17,8 @@ const { resetData } = useResetPasswordStore();
 const { email, token } = storeToRefs(useResetPasswordStore());
 
 const resetPasswordForm = ref({
-    email: email.value ?? '',
     password: '',
     password_confirmation: '',
-    token: token.value ?? '',
 });
 
 const router = useRouter();
@@ -32,7 +31,7 @@ const handleResetPassword = async () => {
         isLoading.value = true;
         await sendPasswordReset('/v1/auth/reset-password', {
             method: 'POST',
-            body: JSON.stringify({ email: resetPasswordForm.value.email, token: resetPasswordForm.value.token, password: resetPasswordForm.value.password, password_confirmation: resetPasswordForm.value.password_confirmation }),
+            body: JSON.stringify({ email: email.value, token: token.value, password: resetPasswordForm.value.password, password_confirmation: resetPasswordForm.value.password_confirmation }),
         });
         resetData();
         await router.push("/login");
@@ -45,29 +44,25 @@ const handleResetPassword = async () => {
 </script>
 
 <template>
-    <div class="flex justify-center items-center h-full lg:h-[700px]">
-        <form @submit.prevent="handleResetPassword" class="bg-white p-8 rounded-lg shadow-2xl max-w-md w-full">
+    <div class="flex justify-center items-center h-full lg:h-[700px] bg-gradient-to-r from-blue-500 to-purple-600">
+        <form @submit.prevent="handleResetPassword"
+            class="bg-white p-8 rounded-lg shadow-2xl max-w-md w-full transform transition-all duration-300 hover:scale-105">
+            <NuxtLink to="/forgot-password" class="absolute top-4 left-4 text-gray-600 hover:text-gray-800">
+                <Icon icon="mdi:arrow-left" width="24" height="24" />
+            </NuxtLink>
             <div class="flex justify-center items-center mb-6">
                 <Icon icon="mdi:lock-reset" :ssr="true" width="96" height="96" class="text-primary" />
             </div>
             <h2 class="text-3xl font-bold text-gray-800 mb-6 text-center">Reset Password</h2>
-            <p class="text-gray-600 mb-6 text-center">Please enter your email, token, new password, and confirm it.</p>
-            <div class="mb-6">
-                <input type="email" v-model="resetPasswordForm.email" placeholder="Email" required
-                    class="w-full px-4 py-2 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" />
-            </div>
+            <p class="text-gray-600 mb-6 text-center">Please enter your new password for <strong>{{ maskEmail(email) }}</strong></p>
             <div class="mb-6">
                 <input type="password" v-model="resetPasswordForm.password" placeholder="New Password" required
-                    class="w-full px-4 py-2 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                    class="w-full px-4 py-2 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-200" />
             </div>
             <div class="mb-6">
                 <input type="password" v-model="resetPasswordForm.password_confirmation" placeholder="Confirm Password"
                     required
-                    class="w-full px-4 py-2 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" />
-            </div>
-            <div class="mb-6">
-                <input type="text" v-model="resetPasswordForm.token" placeholder="Token" required
-                    class="w-full px-4 py-2 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                    class="w-full px-4 py-2 text-lg border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-200" />
             </div>
             <Button text="Reset Password" background="primary" foreground="white" :is-loading="isLoading"
                 :is-wide="true" type="submit"></Button>
