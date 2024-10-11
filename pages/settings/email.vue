045afe95ec60d431ca3786a3b9f96aa974aa3d29
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2024-10-11 12:14:55 +0800

    added: otp timer

--- pages/settings/email.vue
+++ pages/settings/email.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { authStore } from '~/store/auth';
 import type { ApiErrorResponse } from '~/types/api/response/error';
 import { maskEmail } from '~/utils/helper';
+import { useSubmit } from '~/composables/useSubmit';
 
 const tabs = [
     { name: 'Profile', href: '/settings/profile', current: false },
@@ -21,9 +22,26 @@ const maskedEmail = computed(() => maskEmail(user.value.email));
 
 const isLoading = ref(false);
 const isModalOpen = ref(false);
+const isResendDisabled = ref(false);
+const resendTimer = ref(60);
+
+const resendOTPText = computed(() => {
+    return isResendDisabled.value ? `Send OTP (${resendTimer.value}s)` : 'Send OTP';
+});
 
 const { sendRequest: sendRequest } = useSubmit<{ data: { expires_at: string } }, ApiErrorResponse>();
 
+const startResendTimer = () => {
+    isResendDisabled.value = true;
+    resendTimer.value = 60;
+    const interval = setInterval(() => {
+        resendTimer.value -= 1;
+        if (resendTimer.value <= 0) {
+            clearInterval(interval);
+            isResendDisabled.value = false;
+        }
+    }, 1000);
+};
 
 const sendEmailVerification = async () => {
     try {
@@ -34,6 +52,7 @@ const sendEmailVerification = async () => {
                 email: changeEmailForm.value.email,
             },
         });
+        startResendTimer();
     } catch (error) {
         console.error('Error sending password reset email:', error);
     } finally {
@@ -57,6 +76,7 @@ const verifyOTP = async () => {
         isLoading.value = false;
     }
 };
+
 </script>
 
 <template>
@@ -97,43 +117,27 @@ const verifyOTP = async () => {
                 Verify Email
             </template>
             <template #content>
-              <p class="text-sm text-gray-500 mb-4">
-                An OTP has been sent to your email. Please enter it below to verify your email address.
-              </p>
-              <div class="sm:col-span-2 mb-4 flex items-center">
-                <label class="text-sm font-medium text-gray-500 w-1/4 text-left">Email</label>
-                <input
-                  v-model="changeEmailForm.email"
-                  type="email"
-                  autocomplete="email"
-                  required
-                  placeholder="Enter New Email"
-                  class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2 bg-gray-100"
-                />
-              </div>
-              <div class="sm:col-span-2 flex items-center">
-                <label class="text-sm font-medium text-gray-500 w-1/4 text-left">OTP</label>
-                <div class="flex flex-row mt-1 text-sm text-gray-900 w-full">
-                  <input
-                    v-model="changeEmailForm.token"
-                    type="text"
-                    autocomplete="one-time-code"
-                    required
-                    placeholder="Enter OTP"
-                    class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2 bg-gray-100"
-                  />
-                  <Button
-                    @click="sendEmailVerification"
-                    class="whitespace-nowrap ml-2"
-                    type="submit"
-                    text="Send OTP"
-                    background="none"
-                    foreground="black"
-                    :is-loading="isLoading"
-                    :is-wide="false"
-                  ></Button>
+                <p class="text-sm text-gray-500 mb-4">
+                    An OTP has been sent to your email. Please enter it below to verify your email address.
+                </p>
+                <div class="sm:col-span-2 mb-4 flex items-center">
+                    <label class="text-sm font-medium text-gray-500 w-1/4 text-left">Email</label>
+                    <input v-model="changeEmailForm.email" type="email" autocomplete="email" required
+                        placeholder="Enter New Email"
+                        class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2 bg-gray-100" />
                 </div>
-              </div>
+                <div class="sm:col-span-2 flex items-center">
+                    <label class="text-sm font-medium text-gray-500 w-1/4 text-left">OTP</label>
+                    <div class="flex flex-row mt-1 text-sm text-gray-900 w-full">
+                        <input v-model="changeEmailForm.token" type="text" autocomplete="one-time-code" required
+                            placeholder="Enter OTP"
+                            class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2 bg-gray-100" />
+                        <Button @click="sendEmailVerification" :disabled="isResendDisabled"
+                            :class="{ 'cursor-not-allowed': isResendDisabled }"
+                            class="whitespace-nowrap ml-2" type="submit" :text="resendOTPText" background="none"
+                            foreground="black" :is-loading="isLoading" :is-wide="false"></Button>
+                    </div>
+                </div>
             </template>
             <template #actions>
                 <div class="flex justify-end space-x-2">
