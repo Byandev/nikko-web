09443dec6108aa1afc7f678274220f59d6d29664
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2024-10-16 20:32:45 +0800

    added upload image preview

--- components/ProfileInfo.vue
+++ components/ProfileInfo.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { Icon } from '@iconify/vue/dist/iconify.js';
 import { authStore } from '@/store/authStore';
 import type { ApiErrorResponse } from '~/types/api/response/error';
@@ -31,13 +31,34 @@ watch(user, (newMedia) => {
 
 const { sendRequest: sendRequest } = useSubmit<{ data: MediaResponse }, ApiErrorResponse>();
 
-const Image = ref<File | null>(null);
+const avatarImage = ref<File | null>(null);
+const avatarImagePreview = ref<string | null>(null);
+const bannerImage = ref<File | null>(null);
+const bannerImagePreview = ref<string | null>(null);
 
-const updateImage = (event: Event) => {
+const updateAvatarImage = (event: Event) => {
     const target = event.target as HTMLInputElement;
     const file = target?.files?.[0];
     if (file) {
-        Image.value = file;
+        avatarImage.value = file;
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            avatarImagePreview.value = e.target?.result?.toString() || '';
+        };
+        reader.readAsDataURL(file);
+    }
+};
+
+const updateBannerImage = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    const file = target?.files?.[0];
+    if (file) {
+        bannerImage.value = file;
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            bannerImagePreview.value = e.target?.result?.toString() || '';
+        };
+        reader.readAsDataURL(file);
     }
 };
 
@@ -45,8 +66,10 @@ const uploadImage = async (type: MediaType) => {
     try {
         isLoading.value = true;
         const formData = new FormData();
-        if (Image.value) {
-            formData.append('file', Image.value);
+        if (type === MediaType.AVATAR && avatarImage.value) {
+            formData.append('file', avatarImage.value);
+        } else if (type === MediaType.BANNER && bannerImage.value) {
+            formData.append('file', bannerImage.value);
         } else {
             console.error('No image selected.');
             return;
@@ -58,7 +81,7 @@ const uploadImage = async (type: MediaType) => {
             body: formData,
         });
 
-        if(type === 'AVATAR') {
+        if (type === MediaType.AVATAR) {
             // Update the avatar
             await sendRequest(`/v1/auth/profile`, {
                 method: 'PUT',
@@ -83,6 +106,8 @@ const uploadImage = async (type: MediaType) => {
     } finally {
         updateUser();
         isLoading.value = false;
+        avatarImagePreview.value = null;
+        bannerImagePreview.value = null;
     }
 };
 </script>
@@ -90,11 +115,12 @@ const uploadImage = async (type: MediaType) => {
 <template>
     <div class="text-center relative mx-auto border border-gray-300 rounded-lg overflow-hidden bg-white p-4">
         <div class="relative group">
-            <img :src="bannerUrl" alt="Banner" class="w-full h-36 object-cover rounded-t-lg" />
+            <img :src="bannerUrl" alt="Banner" class="w-full h-48 object-cover rounded-t-lg" />
             <div class="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-50"></div>
             <div class="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition duration-300">
                 <label class="cursor-pointer text-white">
-                    <div @click="isBannerModalOpen = true" class="bg-white rounded-full p-2 flex items-center justify-center shadow-md">
+                    <div @click="isBannerModalOpen = true"
+                        class="bg-white rounded-full p-2 flex items-center justify-center shadow-md">
                         <Icon icon="ic:outline-edit" width="24" height="24" class="text-primary" />
                     </div>
                 </label>
@@ -103,20 +129,24 @@ const uploadImage = async (type: MediaType) => {
         <div class="relative">
             <div class="absolute -bottom-24 left-1/2 transform -translate-x-1/2 sm:left-8 sm:translate-x-0 group">
                 <img :src="avatarUrl" alt="Avatar" class="w-36 h-36 rounded-full border-4 border-white shadow-lg" />
-                <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
+                <div
+                    class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
                     <label class="cursor-pointer text-white">
-                        <div @click="isAvatarModalOpen = true" class="bg-white rounded-full p-2 flex items-center justify-center shadow-md">
+                        <div @click="isAvatarModalOpen = true"
+                            class="bg-white rounded-full p-2 flex items-center justify-center shadow-md">
                             <Icon icon="ic:outline-edit" width="24" height="24" class="text-primary" />
                         </div>
                     </label>
                 </div>
-                <div class="mt-2 text-lg font-semibold text-white bg-primary p-2 rounded-lg shadow-sm inline-block">$50/hr</div>
+                <div class="mt-2 text-lg font-semibold text-white bg-primary p-2 rounded-lg shadow-sm inline-block">
+                    $50/hr</div>
             </div>
         </div>
         <div class="mt-20 sm:mt-4 mb-4 px-4 flex flex-col sm:flex-row items-center sm:items-start">
             <div class="sm:ml-40 mt-4 sm:mt-0 text-left">
                 <h2 class="text-2xl font-bold text-gray-900">{{ user.first_name }} {{ user.last_name }}</h2>
-                <p class="mt-2 text-md text-gray-600">Joined on {{ new Date(user.created_at).toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }) }}</p>
+                <p class="mt-2 text-md text-gray-600">Joined on {{ new Date(user.created_at).toLocaleString('en-US', {
+                    month: 'long', day: 'numeric', year: 'numeric' }) }}</p>
                 <div class="mt-2 flex items-center space-x-2">
                     <Icon icon="twemoji:flag-for-flag-philippines" width="15" height="15" />
                     <span class="text-sm text-black font-semibold">Philippines</span>
@@ -128,10 +158,13 @@ const uploadImage = async (type: MediaType) => {
             <template #title>Avatar</template>
             <template #content>
                 <p class="text-sm text-gray-500 mb-4">Please select an image file to upload as your avatar.</p>
+                <div v-if="avatarImagePreview" class="sm:col-span-2 mb-4 flex items-center justify-center">
+                    <img :src="avatarImagePreview" alt="Image Preview" class="w-32 h-32 object-cover rounded-md" />
+                </div>
                 <div class="sm:col-span-2 mb-4 flex items-center">
                     <label class="text-sm font-medium text-gray-500 w-1/4 text-left">Image</label>
-                    <input type="file" accept="image/*" required @change="event => updateImage(event)"
-                        class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2 bg-gray-100" />
+                    <input type="file" accept="image/*" required @change="updateAvatarImage"
+                        class="mt-2 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-primary file:text-white hover:cursor-pointer" />
                 </div>
             </template>
             <template #actions>
@@ -148,10 +181,13 @@ const uploadImage = async (type: MediaType) => {
             <template #title>Upload Banner</template>
             <template #content>
                 <p class="text-sm text-gray-500 mb-4">Please select an image file to upload as your banner.</p>
+                <div v-if="bannerImagePreview" class="sm:col-span-2 mb-4 flex items-center justify-center">
+                    <img :src="bannerImagePreview" alt="Banner Preview" class="w-full h-36 object-cover rounded-md" />
+                </div>
                 <div class="sm:col-span-2 mb-4 flex items-center">
                     <label class="text-sm font-medium text-gray-500 w-1/4 text-left">Image</label>
-                    <input type="file" accept="image/*" required @change="event => updateImage(event)"
-                        class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2 bg-gray-100" />
+                    <input type="file" accept="image/*" required @change="updateBannerImage"
+                        class="mt-2 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-primary file:text-white hover:cursor-pointer" />
                 </div>
             </template>
             <template #actions>
