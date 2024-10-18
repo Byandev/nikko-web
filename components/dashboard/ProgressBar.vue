dd906e5a2d3a6f407fbe215a9b3e05810c3ec121
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2024-10-18 16:47:33 +0800

    FIX

--- components/dashboard/ProgressBar.vue
+++ components/dashboard/ProgressBar.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
 import { computed, defineProps } from 'vue';
 
-interface Step {
-  label: string;
-  state: 'completed' | 'current' | 'upcoming';
-}
-
 const props = defineProps<{
-  steps: Step[];
+  steps: string[];
+  currentStep: number;
 }>();
 
 const stepClasses = computed(() => {
@@ -15,13 +11,13 @@ const stepClasses = computed(() => {
     let stepClass = '';
     let lineClass = '';
 
-    if (step.state === 'completed') {
+    if (index < props.currentStep) {
       stepClass = 'bg-primary text-white';
       lineClass = 'bg-primary';
-    } else if (step.state === 'current') {
+    } else if (index === props.currentStep) {
       stepClass = 'border-2 border-primary bg-primary text-white';
       lineClass = 'bg-primary';
-    } else if (step.state === 'upcoming') {
+    } else if (index > props.currentStep) {
       stepClass = 'border-2 border-primary bg-white text-primary hover:border-primary';
       lineClass = 'bg-primary';
     }
@@ -42,16 +38,16 @@ const stepClasses = computed(() => {
         <div v-if="!stepClasses[index].isLast" class="absolute inset-0 flex items-center" aria-hidden="true">
           <div :class="`h-0.5 w-full ${stepClasses[index].lineClass}`"></div>
         </div>
-        <a href="#" :class="`relative flex h-8 w-8 items-center justify-center rounded-full ${stepClasses[index].stepClass}`" :aria-current="step.state === 'current' ? 'step' : undefined">
-          <template v-if="step.state === 'completed'">
+        <a href="#" :class="`relative flex h-8 w-8 items-center justify-center rounded-full ${stepClasses[index].stepClass}`">
+          <template v-if="index < currentStep">
             <svg class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 0 1 .143 1.052l-8 10.5a.75.75 0 0 1-1.127.075l-4.5-4.5a.75.75 0 0 1 1.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 0 1 1.05-.143Z" clip-rule="evenodd" />
             </svg>
           </template>
           <template v-else>
-            <span :class="step.state === 'upcoming' ? 'text-primary' : 'text-white'">{{ index + 1 }}</span>
+            <span :class="index > currentStep ? 'text-primary' : 'text-white'">{{ index + 1 }}</span>
           </template>
-          <span class="sr-only">{{ step.label }}</span>
+          <span class="sr-only">{{ step }}</span>
         </a>
       </li>
     </ol>
--- pages/onboarding/[accountId].vue
+++ pages/onboarding/[accountId].vue
@@ -7,27 +7,27 @@ import EducationHistoryForm from "~/components/multi-step-forms/EducationHistory
 import SkillsForm from "~/components/multi-step-forms/SkillsForm.vue";
 import ProfileForm from "~/components/multi-step-forms/ProfileForm.vue";
 
-const currentStep = ref(1);
+const currentStep = ref(0);
 </script>
 
 <template>
   <div class="my-5 max-w-2xl mx-5 lg:mx-auto border border-gray-300 rounded-lg py-10 px-4 sm:px-16">
-<!--    <div class="flex justify-center mb-5">-->
-<!--      <ProgressBar :steps="steps" />-->
-<!--    </div>-->
-    <div v-if="currentStep === 1">
+    <div class="flex justify-center mb-5">
+      <ProgressBar :current-step="currentStep" :steps="['Profile', 'Languages', 'Skills', 'Work Experience', 'Education']" />
+    </div>
+    <div v-if="currentStep === 0">
       <ProfileForm @submit="currentStep++" />
     </div>
-    <div v-if="currentStep === 2">
+    <div v-if="currentStep === 1">
       <LanguagesForm @back="currentStep--" @submit="currentStep++" />
     </div>
-    <div v-if="currentStep === 3">
+    <div v-if="currentStep === 2">
       <SkillsForm @back="currentStep--" @submit="currentStep++" />
     </div>
-    <div v-if="currentStep === 4">
+    <div v-if="currentStep === 3">
       <WorkExperiencesForm @back="currentStep--" @submit="currentStep++" />
     </div>
-    <div v-if="currentStep === 5">
+    <div v-if="currentStep === 4">
       <EducationHistoryForm @back="currentStep--" @submit="currentStep++" />
     </div>
 
