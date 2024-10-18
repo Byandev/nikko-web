6306706b6b4fe4548704001bddc0264d214d7df7
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2024-10-18 16:18:21 +0800

    state

--- pages/sign-up/contact-info.vue
+++ pages/sign-up/contact-info.vue
@@ -148,12 +148,12 @@ const handleSubmit = async () => {
             State
           </label>
           <div class="mt-2">
-            <select @change="form.city = ''" v-model="form.state_code"
-                    class="w-full px-2 block text-sm leading-6 rounded-md border-0 py-2 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
-              <option :value="state.isoCode" class="truncate text-sm leading-6 " v-for="state in stateOptions" :key="state.isoCode">
-                {{ state.name }}
-              </option>
-            </select>
+            <div
+                class="flex flex-row items-center px-2 rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+              <Icon icon="mdi:map-marker" :ssr="true" />
+              <input type="text" id="state-code" name="state-code" v-model="form.state_code" required
+                     class="block w-full px-2 placeholder:text-gray-400 sm:text-sm sm:leading-6 outline-none ring-0">
+            </div>
           </div>
         </div>
       </div>
