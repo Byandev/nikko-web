a0d2ac246e9444e2a3e3560aac20fa680b8b661e
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2024-10-18 16:17:29 +0800

    WIP

--- pages/sign-up/contact-info.vue
+++ pages/sign-up/contact-info.vue
@@ -134,13 +134,12 @@ const handleSubmit = async () => {
             Country <span class="text-red-500">*</span>
           </label>
           <div class="mt-2">
-            <select required @change="form.state_code = ''" v-model="form.country_code"
-                    class="w-full px-2 block text-sm leading-6 rounded-md border-0 py-2 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
-              <option :value="country.isoCode" class="truncate text-sm leading-6 " v-for="country in countriesOptions"
-                      :key="country.isoCode">
-                {{ country.name }}
-              </option>
-            </select>
+            <div
+                class="flex flex-row items-center px-2 rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+              <Icon icon="mdi:earth" :ssr="true" />
+              <input type="text" id="country" name="country" v-model="form.country_code" required
+                     class="block w-full px-2 placeholder:text-gray-400 sm:text-sm sm:leading-6 outline-none ring-0">
+            </div>
           </div>
         </div>
 
