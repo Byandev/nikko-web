3ed51ae59c657058bba5ed6d05f97660b3b7b07a
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2024-11-05 20:43:37 +0800

    wip

--- pages/find-freelancer/index.vue
+++ pages/find-freelancer/index.vue
@@ -7,9 +7,18 @@ import type { ApiErrorResponse } from '~/types/api/response/error';
 import type { Freelancer } from '~/types/models/Freelancer';
 import type { PaginatedList } from '~/types/models/Pagination';
 import { debounce } from '~/utils/debounce';
-import { ref } from 'vue'
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
+import { ref, onMounted, watch, computed } from 'vue';
+import { useRouter } from 'vue-router';
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from '@headlessui/vue';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid';
 import type { Skill } from '~/types/models/Skill';
+import { Country} from 'country-state-city';
+import type { ICountry } from 'country-state-city'
 
 
 const router = useRouter();
@@ -41,14 +50,21 @@ const { data: Unsavedfreelancers, fetchData: fetchUnsavedFreelancer, pending: is
 
 const { data: skills, fetchData: fetchSkills, pending: isSkillsLoading } = useFetchData<{ data: Skill[] }, ApiErrorResponse>();
 
-const selectedSkills = ref<Skill | null>(null);
+const selectedSkills = ref<Skill[]>([]);
+const selectedCountries = ref<ICountry[]>([]);
+
+watch(
+  () => selectedSkills.value,
+  (newValue) => {
+    console.log(newValue);
+  }
+);
 
 const { sendRequest: updateFreelancer } = useSubmit<{ data: Freelancer }, ApiErrorResponse>();
 
 onMounted(async () => {
   await fetchFreelancers(1);
   await fetchSkills('/v1/skills');
-  selectedSkills.value = skills.value?.data[0] || null;
 });
 
 watch(
@@ -71,7 +87,7 @@ const fetchFreelancers = async (page: number) => {
   );
 
   await fetchSavedFreelancer(
-    `v1/accounts?include=${searchParams.value.include}&filter[type]=${searchParams.value.type}&filter[search]=${searchParams.value.search}&filter[is_saved]=true$page=${page}`,
+    `v1/accounts?include=${searchParams.value.include}&filter[type]=${searchParams.value.type}&filter[search]=${searchParams.value.search}&filter[is_saved]=true&page=${page}`,
     {
       headers: account?.value?.id
         ? {
@@ -154,6 +170,14 @@ const tabCount = computed(() => {
   };
 });
 
+const removeSkill = (skill: Skill) => {
+  selectedSkills.value = selectedSkills.value.filter(s => s.id !== skill.id);
+};
+
+const removeCountry = (country: ICountry) => {
+  selectedCountries.value = selectedCountries.value.filter(c => c.name !== country.name);
+};
+
 </script>
 
 <template>
@@ -187,39 +211,123 @@ const tabCount = computed(() => {
             </div>
           </template>
           <template #content>
-            <HeadlessListbox v-model="selectedSkills">
+            <Listbox v-model="selectedSkills" multiple>
               <div class="relative mt-1">
-                <HeadlessListboxButton
+                <ListboxButton
                   class="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-primary focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-                  <span class="block truncate">{{ selectedSkills?.name }}</span>
+                  <span class="block truncate">
+                    <span v-if="selectedSkills.length === 0">Select Skills</span>
+                    <span v-else>
+                      <span v-for="(skill, index) in selectedSkills" :key="skill.id">
+                        {{ skill.name }}<span v-if="index < selectedSkills.length - 1">, </span>
+                      </span>
+                    </span>
+                  </span>
                   <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                     <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
                   </span>
-                </HeadlessListboxButton>
+                </ListboxButton>
 
                 <transition leave-active-class="transition duration-100 ease-in" leave-from-class="opacity-100"
                   leave-to-class="opacity-0">
-                  <HeadlessListboxOptions
-                    class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base ring-1 ring-primary ring-opacity-5 focus:outline-none sm:text-sm">
-                    <HeadlessListboxOption v-for="skill in skills?.data" v-slot="{ active, selected }" :key="skill.name"
+                  <ListboxOptions
+                    class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base ring-1 ring-primary ring-opacity-5 focus:outline-none sm:text-sm z-50">
+                    <ListboxOption v-for="skill in skills?.data" v-slot="{ active, selected }" :key="skill.id"
                       :value="skill" as="template">
-                      <li :class="[
-                        active ? 'bg-primary/10 text-primary' : 'text-gray-900',
-                        'relative cursor-default select-none py-2 pl-10 pr-4',
-                      ]">
-                        <span :class="[
-                          selected ? 'font-medium' : 'font-normal',
-                          'block truncate',
-                        ]">{{ skill.name }}</span>
+                      <li
+                        :class="[active ? 'bg-primary/10 text-primary' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-10 pr-4']">
+                        <span :class="[selected ? 'font-medium' : 'font-normal', 'block truncate']">{{ skill.name
+                          }}</span>
                         <span v-if="selected" class="absolute inset-y-0 left-0 flex items-center pl-3 text-primary">
                           <CheckIcon class="h-5 w-5" aria-hidden="true" />
                         </span>
                       </li>
-                    </HeadlessListboxOption>
-                  </HeadlessListboxOptions>
+                    </ListboxOption>
+                  </ListboxOptions>
                 </transition>
               </div>
-            </HeadlessListbox>
+            </Listbox>
+
+            <div class="mt-4 flex flex-wrap">
+              <div v-for="skill in selectedSkills" :key="skill.id" class="mr-2 my-1">
+                <span
+                  class="inline-flex items-center gap-x-0.5 rounded-md bg-green-50 px-2 py-1 text-sm font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                  {{ skill.name }}
+                  <button @click="removeSkill(skill)" type="button"
+                    class="group relative -mr-1 h-3.5 w-3.5 rounded-sm hover:bg-green-600/20">
+                    <span class="sr-only">Remove</span>
+                    <svg viewBox="0 0 14 14" class="h-3.5 w-3.5 stroke-green-700/50 group-hover:stroke-green-700/75">
+                      <path d="M4 4l6 6m0-6l-6 6" />
+                    </svg>
+                    <span class="absolute -inset-1" />
+                  </button>
+                </span>
+              </div>
+            </div>
+          </template>
+        </Section>
+
+        <!-- Country Section -->
+        <Section>
+          <template #header>
+            <div class="flex justify-between items-center">
+              <h2 class="text-xl font-bold">Country</h2>
+            </div>
+          </template>
+          <template #content>
+            <Listbox v-model="selectedCountries" multiple>
+              <div class="relative mt-1">
+                <ListboxButton
+                  class="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-primary focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                  <span class="block truncate">
+                    <span v-if="selectedCountries.length === 0">Select Country</span>
+                    <span v-else>
+                      <span v-for="(country, index) in selectedCountries" :key="index">
+                        {{ country.name }}<span v-if="index < selectedCountries.length - 1">, </span>
+                      </span>
+                    </span>
+                  </span>
+                  <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                    <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                  </span>
+                </ListboxButton>
+
+                <transition leave-active-class="transition duration-100 ease-in" leave-from-class="opacity-100"
+                  leave-to-class="opacity-0">
+                  <ListboxOptions
+                    class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base ring-1 ring-primary ring-opacity-5 focus:outline-none sm:text-sm z-50">
+                    <ListboxOption v-for="(country, index) in Country.getAllCountries()" v-slot="{ active, selected }" :key="index"
+                      :value="country" as="template">
+                      <li
+                        :class="[active ? 'bg-primary/10 text-primary' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-10 pr-4']">
+                        <span :class="[selected ? 'font-medium' : 'font-normal', 'block truncate']">{{ country.name
+                          }}</span>
+                        <span v-if="selected" class="absolute inset-y-0 left-0 flex items-center pl-3 text-primary">
+                          <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      </li>
+                    </ListboxOption>
+                  </ListboxOptions>
+                </transition>
+              </div>
+            </Listbox>
+
+            <div class="mt-4 flex flex-wrap">
+              <div v-for="(country, index) in selectedCountries" :key="index" class="mr-2 my-1">
+                <span
+                  class="inline-flex items-center gap-x-0.5 rounded-md bg-green-50 px-2 py-1 text-sm font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                  {{ country.name }}
+                  <button @click="removeCountry(country)" type="button"
+                    class="group relative -mr-1 h-3.5 w-3.5 rounded-sm hover:bg-green-600/20">
+                    <span class="sr-only">Remove</span>
+                    <svg viewBox="0 0 14 14" class="h-3.5 w-3.5 stroke-green-700/50 group-hover:stroke-green-700/75">
+                      <path d="M4 4l6 6m0-6l-6 6" />
+                    </svg>
+                    <span class="absolute -inset-1" />
+                  </button>
+                </span>
+              </div>
+            </div>
           </template>
         </Section>
 
