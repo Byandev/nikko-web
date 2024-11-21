1dd4a068887eaf079f7f62897afc1c8ba5b4f074
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2024-11-21 16:04:25 +0800

    fix header errors

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@ console.log(status.value)
 
 <template>
   <div class="h-screen flex flex-col">
-    <Header class="sticky top-0 z-50" />
+    <Header :class="'sticky top-0 z-50'" />
     <div class="flex-1 overflow-auto" v-if="status !== 'loading'">
       <slot />
     </div>
