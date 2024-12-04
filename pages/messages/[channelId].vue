f7ef63b5e065d10a250393296aefef5e6660b311
Kyle <kyleanthony47@gmail.com>
2024-12-04 23:24:40 +0800

    wip

--- pages/messages/[channelId].vue
+++ pages/messages/[channelId].vue
@@ -8,8 +8,9 @@ import { formatDayTime } from '~/utils/formatter';
 
 const { account } = storeToRefs(accountStore());
 
-const { data: channels, fetchData: fetchChannels } = useFetchData<{ data: Channel[] }, ApiErrorResponse>();
-const { data: messages, fetchData: fetchMessages, pending: isLoading } = useFetchData<{ data: Message[] }, ApiErrorResponse>();
+const { data: channels, fetchData: fetchChannels, pending: isChannelLoading } = useFetchData<{ data: Channel[] }, ApiErrorResponse>();
+const { data: messages, fetchData: fetchMessages, pending: isMessagesLoading } = useFetchData<{ data: Message[] }, ApiErrorResponse>();
+const { sendRequest: sendMesage, pending: isSending } = useSubmit<{ data: Message }, ApiErrorResponse>();
 
 const requestHeaders = computed<HeadersInit | undefined>(() =>
     account.value?.id ? { 'X-ACCOUNT-ID': account.value.id.toString() } : undefined
@@ -40,6 +41,26 @@ onMounted(async () => {
         message.value = messages.value.data
     }
 });
+
+const handleMessageSubmit = async () => {
+    if (newMessage.value) {
+        await sendMesage(`/v1/chat/channels/${route.params.channelId}/messages`, {
+            method: 'POST',
+            headers: requestHeaders.value,
+            body: {
+                content: newMessage.value
+            }
+        });
+        newMessage.value = '';
+        await fetchMessages(`/v1/chat/channels/${route.params.channelId}/messages`, {
+            headers: requestHeaders.value
+        });
+
+        if (messages.value && messages.value.data) {
+            message.value = messages.value.data
+        }
+    }
+};
 
 const selectChat = async (id: number) => {
     await router.push(`/messages/${id}`);
@@ -76,19 +97,19 @@ const name = computed(() => {
                 <button @click="router.push('/messages')" class="mr-4 p-2 bg-gray-200 rounded-full hover:bg-gray-300">
                     <Icon icon="mdi:arrow-left" class="w-5 h-5" />
                 </button>
-                <img v-if="chats && !isLoading && activeChannel"
-                    :src="avatar"
-                    alt="User" class="w-10 h-10 rounded-full mr-4" />
+                <img v-if="chats && !isChannelLoading && activeChannel" :src="avatar" alt="User"
+                    class="w-10 h-10 rounded-full mr-4" />
                 <div v-else>
                     <div class="w-10 h-10 bg-gray-300 rounded-full"></div>
                 </div>
                 <div class="flex justify-between w-full">
                     <div class="flex-1">
-                        <div v-if="chats && !isLoading && activeChannel" class="text-lg font-semibold flex flex-col">
+                        <div v-if="chats && !isChannelLoading && activeChannel"
+                            class="text-lg font-semibold flex flex-col">
                             <span>{{ name }}</span>
                             <span v-if="activeChannel" class="text-xs text-gray-500">{{
                                 timeAgo(activeChannel?.last_activity_at)
-                                }}</span>
+                            }}</span>
                         </div>
                         <div v-else>
                             <div class="ml-5 w-20 h-4 bg-gray-300 rounded"></div>
@@ -110,7 +131,7 @@ const name = computed(() => {
             </div>
 
             <!-- Chat Messages -->
-            <div v-if="message && message[0] && !isLoading" class="flex-1 p-4 overflow-y-auto">
+            <div v-if="message && message[0] && !isMessagesLoading" class="flex-1 p-4 overflow-y-auto flex-grow">
                 <div class="space-y-4">
                     <div v-for="(item, index) in message" :key="index">
                         <div v-if="item.sender.first_name === 'other'" class="flex justify-start">
@@ -151,7 +172,8 @@ const name = computed(() => {
         <div v-if="currentTab == 'gallery-section'"
             class="lg:w-2/3 bg-white flex flex-col h-full border-l-2 border-r-2">
             <div class="flex items-center justify-start p-4 bg-gray-50 border-b">
-                <button @click="{currentTab = 'chat-section';
+                <button @click="{
+                    currentTab = 'chat-section';
                     showDropdown = false;
                 }" class="mr-4 p-2 bg-gray-200 rounded-full hover:bg-gray-300">
                     <Icon icon="mdi:arrow-left" class="w-5 h-5" />
@@ -189,7 +211,7 @@ const name = computed(() => {
 
                 <!-- Chat List -->
                 <div class="overflow-y-auto flex-grow">
-                    <div v-if="isLoading" class="animate-pulse space-y-2" v-for="n in 2" :key="n">
+                    <div v-if="isChannelLoading" class="animate-pulse space-y-2" v-for="n in 2" :key="n">
                         <div class="flex items-center p-4 border-b">
                             <div class="w-12 h-12 bg-gray-200 rounded-full mr-4 animate-pulse"></div>
                             <div class="flex-1">
@@ -199,13 +221,15 @@ const name = computed(() => {
                             <div class="h-4 bg-gray-200 rounded w-12 animate-pulse"></div>
                         </div>
                     </div>
-                    <div v-if="chats && !isLoading" v-for="chat in chats" :key="chat.id" @click="selectChat(chat.id)"
-                        :class="['flex items-center p-4 border-b cursor-pointer', chat.id == Number(route.params.channelId as string)? 'bg-gray-200': 'bg-white']">
-                        <img :src="activeChannel?.members.find(member=> account?.id != member.id)?.avatar.original_url" alt="User"
-                            class="w-12 h-12 rounded-full mr-4" />
+                    <div v-if="chats && !isChannelLoading" v-for="chat in chats" :key="chat.id"
+                        @click="selectChat(chat.id)"
+                        :class="['flex items-center p-4 border-b cursor-pointer', chat.id == Number(route.params.channelId as string) ? 'bg-gray-200' : 'bg-white']">
+                        <img :src="activeChannel?.members.find(member => account?.id != member.id)?.avatar.original_url"
+                            alt="User" class="w-12 h-12 rounded-full mr-4" />
                         <div class="flex-1">
-                            <div class="text-lg font-semibold">{{ activeChannel?.members.find(member => account?.id != member.id)?.first_name }} {{
-                                 activeChannel?.members.find(member => account?.id != member.id)?.last_name }}</div>
+                            <div class="text-lg font-semibold">{{ activeChannel?.members.find(member => account?.id !=
+                                member.id)?.first_name }} {{
+                                    activeChannel?.members.find(member => account?.id != member.id)?.last_name }}</div>
                         </div>
                         <div class="text-xs text-gray-500">{{ timeAgo(chat.created_at) }}</div>
                     </div>
@@ -214,20 +238,22 @@ const name = computed(() => {
 
             <!-- Chat Section -->
             <div class="w-full lg:w-2/3 bg-white flex flex-col h-full border-l-2 border-r-2">
+
                 <!-- Chat Header -->
                 <div class="flex items-center p-4 bg-gray-50 border-b">
-                    <img v-if="chats && !isLoading && activeChannel"
+                    <img v-if="chats && !isChannelLoading && activeChannel"
                         :src="activeChannel?.members.find(member => account?.id != member.id)?.avatar.original_url"
                         alt="User" class="w-10 h-10 rounded-full mr-4" />
                     <div v-else>
                         <div class="w-10 h-10 bg-gray-300 rounded-full animate-pulse"></div>
                     </div>
                     <div class="flex-1">
-                        <div v-if="chats && !isLoading && activeChannel" class="text-lg font-semibold flex flex-col">
+                        <div v-if="chats && !isChannelLoading && activeChannel"
+                            class="text-lg font-semibold flex flex-col">
                             <span>{{ name }}</span>
                             <span v-if="activeChannel" class="text-xs text-gray-500">{{
                                 timeAgo(activeChannel?.last_activity_at)
-                                }}</span>
+                            }}</span>
                         </div>
                         <div v-else>
                             <div class="ml-5 w-20 h-4 bg-gray-300 rounded animate-pulse"></div>
@@ -236,17 +262,20 @@ const name = computed(() => {
                     </div>
                 </div>
 
-                <div v-if="message && message[0] && !isLoading" class="flex flex-col flex-grow h-full">
+                <div v-if="message && message[0] && !isMessagesLoading" class="flex flex-col h-full">
+
                     <!-- Chat Messages -->
-                    <div class="flex-1 p-4 overflow-y-auto">
-                        <div class="space-y-4">
+                    <div class="flex-1 p-4 overflow-y-auto h-20">
+                        <div class="flex flex-col gap-2 h-1/2">
                             <div v-for="(item, index) in message" :key="index">
+                                <!-- Received Messages -->
                                 <div v-if="item.sender.id !== account?.id" class="flex justify-start">
                                     <div class="bg-white p-3 rounded-lg shadow w-max">
                                         <p>{{ item.content }}</p>
                                         <span class="text-xs text-gray-500">{{ formatDayTime(item.created_at) }}</span>
                                     </div>
                                 </div>
+                                <!-- Sent Messages -->
                                 <div v-else class="flex justify-end">
                                     <div class="relative group flex flex-row items-center gap-2">
                                         <span
@@ -265,21 +294,21 @@ const name = computed(() => {
                 <div v-else class="flex items-center justify-center flex-grow">
                     <Icon icon="line-md:loading-loop" width="24" height="24" />
                 </div>
-                <!-- Chat Input -->
-                <div class="flex items-center p-2 bg-gray-100 border-t">
-                    <input type="text" v-model="newMessage" placeholder="Aa"
-                        class="flex-1 p-2 border rounded-lg focus:outline-none focus:ring focus:ring-green-400" />
-                    <button @click="" class="ml-4 p-2 bg-primary text-white rounded-full hover:bg-primary/80">
-                        <Icon icon="mdi:send" class="w-5 h-5" />
-                    </button>
-                </div>
+                 <!-- Chat Input -->
+                 <div class="flex items-center p-2 bg-gray-100 border-t">
+                        <input type="text" v-model="newMessage" placeholder="Aa"
+                            class="flex-1 p-2 border rounded-lg focus:outline-none focus:ring focus:ring-green-400" />
+                        <button @click="handleMessageSubmit"
+                            class="ml-4 p-2 bg-primary text-white rounded-full hover:bg-primary/80">
+                            <Icon :icon="!isSending ? 'mdi:send' : 'line-md:loading-loop'" class="w-5 h-5" />
+                        </button>
+                    </div>
             </div>
 
             <!-- Profile Section -->
             <div class="w-full lg:w-1/3 bg-gray-50 flex flex-col h-full p-4">
-                <div v-if="chats && !isLoading" class="flex flex-col items-center">
-                    <img v-if="activeChannel" :src="avatar" alt="User"
-                        class="w-24 h-24 rounded-full animate-pulse" />
+                <div v-if="chats && !isChannelLoading" class="flex flex-col items-center">
+                    <img v-if="activeChannel" :src="avatar" alt="User" class="w-24 h-24 rounded-full animate-pulse" />
                     <div v-else>
                         <div class="w-24 h-24 bg-gray-300 rounded-full animate-pulse"></div>
                     </div>
