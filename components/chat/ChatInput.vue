ff22e0a2adb9c56c53c5f25607e66c35658cd2f8
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2024-12-14 16:50:02 +0800

    fix

--- components/chat/ChatInput.vue
+++ components/chat/ChatInput.vue
@@ -4,6 +4,7 @@ import type {ApiErrorResponse} from "~/types/api/response/error";
 import type {Media} from "~/types/models/Media";
 import type {Message} from "~/types/models/Message";
 import {accountStore} from "~/store/accountStore";
+import { ref } from 'vue';
 
 const props = defineProps<{
     channelId: String;
@@ -21,6 +22,19 @@ const attachmentFiles = ref<File[]>([]);
 const newMessage = ref<string>('');
 const attachmentUrls = ref<string[]>([]);
 const attachmentNames = ref<string[]>([]);
+
+const showModal = ref(false);
+const selectedImage = ref<string | null>(null);
+
+const handleImageClick = (imageUrl: string) => {
+    selectedImage.value = imageUrl;
+    showModal.value = true;
+};
+
+const closeModal = () => {
+    showModal.value = false;
+    selectedImage.value = null;
+};
 
 const handleAttachment = (event: Event) => {
     const target = event.target as HTMLInputElement;
@@ -90,7 +104,7 @@ watch(() => attachmentFiles.value, async (newFiles) => {
 <template>
     <div v-if="attachmentFiles && attachmentFiles.length" class="mr-2 p-2 bg-gray-200 text-gray-600 rounded-t-lg flex items-center">
         <div v-for="(file, index) in attachmentFiles" :key="index" class="flex items-center mr-2">
-            <img :src="attachmentUrls[index]" :alt="attachmentNames[index]" class="w-20 h-20 object-cover rounded-lg max-w-[75%]" />
+            <img :src="attachmentUrls[index]" :alt="attachmentNames[index]" class="w-20 h-20 object-cover rounded-lg max-w-[75%]" @click="handleImageClick(attachmentUrls[index])" />
             <button @click="removeAttachment(index)" class="ml-2 text-red-500 hover:text-red-700">
                 <Icon icon="mdi:close" class="w-4 h-4" />
             </button>
@@ -106,5 +120,13 @@ watch(() => attachmentFiles.value, async (newFiles) => {
         <button @click="handleMessageSubmit" class="ml-4 p-2 bg-primary text-white rounded-full hover:bg-primary/80">
             <Icon :icon="!isSending ? 'mdi:send' : 'line-md:loading-loop'" class="w-5 h-5" />
         </button>
+    </div>
+    <div v-if="showModal" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+        <div class="bg-white p-4 rounded-lg max-w-3xl max-h-3xl overflow-auto">
+            <img :src="selectedImage ?? ''" class="max-w-full max-h-full" />
+            <button @click="closeModal" class="mt-2 text-red-500 hover:text-red-700">
+                Close
+            </button>
+        </div>
     </div>
 </template>
